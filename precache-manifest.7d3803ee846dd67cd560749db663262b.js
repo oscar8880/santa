621a138c7e1021f85b8d9cdc36e1c9ee7172add2
Oscar Zealley <oscar@zealley.com>
2019-11-07 14:11:40 +0000

    Updates

--- precache-manifest.7d3803ee846dd67cd560749db663262b.js
+++ precache-manifest.7d3803ee846dd67cd560749db663262b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b65ea2c384db285f0f40594d5210a0a3",
+    "revision": "9525955ab8c9edf6ba2fea60967a23fb",
     "url": "/santa/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/santa/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "3df593062dfd419ba0f6",
+    "revision": "68fbd142643d216d8ee5",
     "url": "/santa/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/santa/static/js/2.8bb9dc91.chunk.js"
   },
   {
-    "revision": "3df593062dfd419ba0f6",
-    "url": "/santa/static/js/main.421906ab.chunk.js"
+    "revision": "68fbd142643d216d8ee5",
+    "url": "/santa/static/js/main.a5c3228f.chunk.js"
   },
   {
     "revision": "989085c84395fb0e7860",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/santa/precache-manifest.643227eb74834f530da5ccbcaf98f0cd.js"
+  "/santa/precache-manifest.7d3803ee846dd67cd560749db663262b.js"
 );
 
 self.addEventListener('message', (event) => {
