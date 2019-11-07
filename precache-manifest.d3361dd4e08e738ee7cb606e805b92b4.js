0c583f8765460c917d4da4bedd622d97c4413672
Oscar Zealley <oscar@zealley.com>
2019-11-07 10:22:16 +0000

    Updates

---
+++ precache-manifest.d3361dd4e08e738ee7cb606e805b92b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833c5b3d93fde5b68f238ab851ea0de7",
+    "url": "/santa/index.html"
+  },
+  {
+    "revision": "43f23fa671a095e929ae",
+    "url": "/santa/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4727741514a1e6f8fb72",
+    "url": "/santa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "43f23fa671a095e929ae",
+    "url": "/santa/static/js/2.8bb9dc91.chunk.js"
+  },
+  {
+    "revision": "4727741514a1e6f8fb72",
+    "url": "/santa/static/js/main.867264b4.chunk.js"
+  },
+  {
+    "revision": "989085c84395fb0e7860",
+    "url": "/santa/static/js/runtime-main.3d8da218.js"
+  }
+]);
